f71c98c490909bff17595249f7b1ff43f5b688b9
yudhaaaa <141938548+yudha556@users.noreply.github.com>
2025-06-19 22:10:31 +0700

    router list page'

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gofield/app/views/splash/page.dart';
 import 'package:gofield/core/router/app_routes.dart';
